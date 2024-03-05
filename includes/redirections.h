31db6f9178799d2c2933938c117e5b8617103cf0
Alec Croue <alec.croue@gmail.com>
2024-03-05 11:09:01 +0100

    added open_outfiles to redirections.h

--- includes/redirections.h
+++ includes/redirections.h
@@ -6,7 +6,7 @@
 /*   By: acroue <acroue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 16:16:50 by acroue            #+#    #+#             */
-/*   Updated: 2024/03/04 17:14:40 by acroue           ###   ########.fr       */
+/*   Updated: 2024/03/05 10:12:15 by acroue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include "minishell.h"
 
 int		open_infiles(t_branch **branch);
-int	open_outfiles(t_branch **tree);
+int		open_outfiles(t_branch **tree);
 
 #endif
