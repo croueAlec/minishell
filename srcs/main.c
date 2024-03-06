181f9fde5cd3b21a8cf8f1a3da83623d10bb2d4c
Alec Croue <alec.croue@gmail.com>
2024-03-06 15:55:09 +0100

    removed execute_tree (it stopped the makefile from compiling)

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: acroue <acroue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 17:13:47 by acroue            #+#    #+#             */
-/*   Updated: 2024/03/05 19:31:23 by acroue           ###   ########.fr       */
+/*   Updated: 2024/03/06 15:54:10 by acroue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@ int	main(int argc, char const *argv[])
 	printf("This is a test\n");
 	print_exec();
 	print_parsing();
-	execute_tree(NULL, NULL);
 	return (0);
 }
