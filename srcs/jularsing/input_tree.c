630c0355390a4c921e6bffa3c6af57fe6115c366
mynameisjul <jublaye@hotmail.fr>
2024-03-27 16:14:41 +0100

    corrected pointer issues to build input tree

--- srcs/jularsing/input_tree.c
+++ srcs/jularsing/input_tree.c
@@ -1,27 +1,51 @@
 #include "minishell.h"
 
+int	str_tab_len(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab && tab[i] != 0)
+		i++;
+	return (i);
+}
+
+t_branch	*new_cmd_branch(char **cmd_split, int hd_fd, char **env)
+{
+	t_branch	*node;
+
+	node = ft_calloc(1, sizeof(t_branch));
+	if (!node)
+		return (NULL);
+	node->type = T_CMD;
+	node->elmnt = new_cmd(cmd_split, hd_fd, env);
+	return (node);
+}
+
+
 t_branch	*input_tree(char **input, int hd_fd, char **env)
 {
 	t_branch	*tree;
-	t_branch	*tmp;
-	t_cmd		*cmd;
+	t_branch	*new_branch;
+	t_cmd		*prev_cmd;
 	char		**cmd_split;
 	int			i;
 
 	i = 0;
 	tree = NULL;
-	tmp = tree;
-	cmd = NULL;
+	new_branch = NULL;
+	prev_cmd = NULL;
 	while (input[i])
 	{
 		cmd_split = quotes_split(input[i], ' ');
 		if (!cmd_split)
 			return (free_tree(tree), NULL);
-		tmp = new_cmd_branch(cmd_split, hd_fd, env);
+		new_branch = new_cmd_branch(cmd_split, hd_fd, env);
+		if (prev_cmd)
+			prev_cmd->next_cmd = new_branch;
 		if (i == 0)
-			tree = tmp;
-		cmd = tmp->elmnt;
-		cmd->next_cmd = tmp;
+			tree = new_branch;
+		prev_cmd = new_branch->elmnt;
 		ft_fsplit(cmd_split);
 		i++;
 	}
--- srcs/jularsing/test.c
+++ srcs/jularsing/test.c
@@ -6,7 +6,7 @@
 /*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 10:41:19 by jblaye            #+#    #+#             */
-/*   Updated: 2024/03/27 14:01:37 by jblaye           ###   ########.fr       */
+/*   Updated: 2024/03/27 15:20:47 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,32 @@
 int	main(int ac, char **av, char **env)
 {
 	t_branch	*tree;
-	t_cmd		*thecmd;
+	//t_cmd		*thecmd;
 
 	(void) ac;
 	(void) av;
 	tree = parsing(env);
 	printf("j ai un arbre\n");
 	printf("tree %p\n", tree);
-	thecmd = tree->elmnt;
-	while (tree)
-	{
-		printf("BRANCH TYPE = %i\n", tree->type);
-		printf("CMD PATH = %s\n", thecmd->cmd_path);
-		printf("CMD ARGS\n");
-		ft_printstrtab(thecmd->args);
-		printf("CMD REDIRECTIONS\n");
-		int i = 0;
-		while (thecmd->tree[i])
-		{
-			printf("REDIRECTION = %s\n", (char *) thecmd->tree[i]->elmnt);
-			i++;
-		}
-		printf("===============\n===============\n");
-		tree = thecmd->next_cmd;
-		thecmd = tree->elmnt;
-	}
+	//thecmd = tree->elmnt;
+	// while (tree)
+	// {
+	// 	printf("BRANCH TYPE = %i\n", tree->type);
+	// 	printf("CMD PATH = %s\n", thecmd->cmd_path);
+	// 	printf("CMD ARGS\n");
+	// 	ft_printstrtab(thecmd->args);
+	// 	printf("CMD REDIRECTIONS\n");
+	// 	int i = 0;
+	// 	while (thecmd->tree[i])
+	// 	{
+	// 		printf("REDIRECTION = %s\n", (char *) thecmd->tree[i]->elmnt);
+	// 		i++;
+	// 	}
+	// 	printf("===============\n===============\n");
+	// 	tree = thecmd->next_cmd;
+	// 	thecmd = tree->elmnt;
+	// }
 	printf("je le print\n");
-	free_tree(tree);
+	//free_tree(tree);
 	return (0);
 }
