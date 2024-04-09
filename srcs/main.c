53905a1a55e2fba1110c3d445778802739d576b0
Alec Croue <alec.croue@gmail.com>
2024-04-09 18:37:15 +0200

    fixed last_pid not being used

--- srcs/main.c
+++ srcs/main.c
@@ -6,22 +6,37 @@
 /*   By: acroue <acroue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 17:13:47 by acroue            #+#    #+#             */
-/*   Updated: 2024/04/09 18:24:37 by acroue           ###   ########.fr       */
+/*   Updated: 2024/04/09 18:36:18 by acroue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	wait_children(int pid)
+{
+	int	wait_status;
+	int	error_status;
+
+	while (errno != ECHILD)
+		if (wait(&wait_status) == pid && WIFEXITED(wait_status))
+			error_status = WEXITSTATUS(wait_status);
+	if (pid == -1)
+		return (127);
+	return (error_status);
+}
+
 int	main(int ac, char **av, char **env)
 {
 	t_branch	*tree;
+	pid_t		last_pid;
 
 	(void) ac;
 	(void) av;
 	while (1)
 	{
 		tree = parsing(env);
-		execute_tree(tree, env, 0);
+		last_pid = execute_tree(tree, env, 0);
+		wait_children(last_pid);
 	}
 	return (0);
 }
