b8a82a0ed63509ef9f35525f401bdf37048e99c1
Alec Croue <alec.croue@gmail.com>
2024-04-11 11:14:16 +0200

    fix mixed versions

--- srcs/jularsing/input_tree.c
+++ srcs/jularsing/input_tree.c
@@ -6,7 +6,7 @@
 /*   By: acroue <acroue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 13:38:44 by jblaye            #+#    #+#             */
-/*   Updated: 2024/04/10 16:34:21 by jblaye           ###   ########.fr       */
+/*   Updated: 2024/04/11 11:13:39 by acroue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ t_branch	*new_cmd_branch(char *cmd, t_hd_fd_list *hd_fd_list, t_env *env)
 	char		**cmd_split;
 	char		*input;
 
-	input = str_expand_var(cmd, env);
+	input = str_expand_var(cmd, env->env_tab);
 	if (!input)
 		return (NULL);
 	cmd_split = quotes_split(input, ' ');
