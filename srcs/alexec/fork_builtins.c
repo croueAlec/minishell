e9d6ba464df4e4024026bafa35add4f74b1cfce1
Alec Croue <alec.croue@gmail.com>
2024-04-11 15:59:12 +0200

    added exit to built-ins execution

--- srcs/alexec/fork_builtins.c
+++ srcs/alexec/fork_builtins.c
@@ -6,7 +6,7 @@
 /*   By: acroue <acroue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 13:48:34 by acroue            #+#    #+#             */
-/*   Updated: 2024/04/11 12:00:34 by acroue           ###   ########.fr       */
+/*   Updated: 2024/04/11 15:51:59 by acroue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ pid_t	fork_built_ins(int fd_out, t_branch *branch, size_t *cmd_number)
 {
 	pid_t	pid;
 	t_cmd	*cmd;
+	int		err_no;
 
 	pid = -1;
 	cmd = branch->elmnt;
@@ -26,6 +27,9 @@ pid_t	fork_built_ins(int fd_out, t_branch *branch, size_t *cmd_number)
 	if (cmd && (cmd->next_cmd || *cmd_number) && pid == 0)
 	{
 		cmd->env->err_no = handle_builtins(branch, cmd_number, fd_out);
+		err_no = cmd->env->err_no;
+		free_tree(branch);
+		exit(err_no);
 	}
 	else if (cmd && (!cmd->next_cmd && !*cmd_number))
 	{
